f1212e28066722c5e6efa2b463e5050436d89ded
Mike . <mike.@Mikes-MacBook-Pro.local>
2026-06-23 21:08:59 -0500

    Fix how-i-work page

---
+++ app/how-i-work/page.tsx
@@ -0,0 +1,8 @@
+export default function HowIWorkPage() {
+  return (
+    <main style={{ padding: "200px 40px" }}>
+      <h1>How I Work</h1>
+      <p>How I Work page under construction.</p>
+    </main>
+  );
+}
